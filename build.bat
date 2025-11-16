67d9dc5de01507aaac3c4a06f7cca0d3a5272797
bimotron <l.stlaurent@bimotron.com>
2025-11-16 11:26:17 -0500

    commit3

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set FILES=index.md BIMCanada.md BigD.md LOD.md LOIN.md LOIN-LODD.md OpenBimLogiciel.md Normes.md
-pandoc %FILES% -s --css style.css -o livre.html
-"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe" --enable-local-file-access livre.html livre.pdf
-echo Done.
-pause
